7d8e3907374dba96ac37a6319fab9672f139c80c
f.batti <fabio.batti93@gmail.com>
2023-10-29 19:11:52 +0100

    extracted guessing game into its own mod%

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
-use rand::Rng;
-use std::cmp::Ordering;
-use std::io;
+mod games;
+use games::game::NumberGuessingGame;
+
+fn main() {
+    NumberGuessingGame::run();
+}
 
 // pub struct Dog {
 //     name: String,
@@ -28,39 +31,3 @@ use std::io;
 // };
 // doggy.personalized_bark()
 // }
-
-fn main() {
-    println!("Guess the number!");
-    let secret_number = rand::thread_rng().gen_range(1..=100);
-    // println!("The secret number is {secret_number}.");
-    loop {
-        println!("Please input your guess.");
-
-        let mut guess = String::new();
-
-        io::stdin()
-            .read_line(&mut guess)
-            .expect("Failed to read line");
-        // this will still crash the program
-        // let guess: u32 = guess.trim().parse().expect("Please input a number");
-
-        let guess: u32 = match guess.trim().parse() {
-            Ok(num) => num,
-            Err(_) => {
-                println!("Your input could not be converted to a number");
-                continue;
-            }
-        };
-
-        println!("You guessed: {guess}");
-
-        match guess.cmp(&secret_number) {
-            Ordering::Equal => {
-                println!("You Win!");
-                break;
-            }
-            Ordering::Less => println!("Too small!"),
-            Ordering::Greater => println!("Too big!"),
-        }
-    }
-}
